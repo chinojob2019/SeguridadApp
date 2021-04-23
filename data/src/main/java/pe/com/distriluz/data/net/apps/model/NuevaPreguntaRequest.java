425152784fb03c6aeff2002d019c9a04b81dee6f
cloayzab <cloayzab@canvia.com>
2021-04-22 19:29:25 -0500

    Listado de Preguntas Frecuentes

---
+++ data/src/main/java/pe/com/distriluz/data/net/apps/model/NuevaPreguntaRequest.java
@@ -0,0 +1,50 @@
+package pe.com.distriluz.data.net.apps.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class NuevaPreguntaRequest{
+
+	@SerializedName("descripcion")
+	private String descripcion;
+
+	@SerializedName("idEstado")
+	private int idEstado;
+
+	@SerializedName("orden")
+	private int orden;
+
+	@SerializedName("idPregunta")
+	private int idPregunta;
+
+	public void setDescripcion(String descripcion){
+		this.descripcion = descripcion;
+	}
+
+	public String getDescripcion(){
+		return descripcion;
+	}
+
+	public void setIdEstado(int idEstado){
+		this.idEstado = idEstado;
+	}
+
+	public int getIdEstado(){
+		return idEstado;
+	}
+
+	public void setOrden(int orden){
+		this.orden = orden;
+	}
+
+	public int getOrden(){
+		return orden;
+	}
+
+	public void setIdPregunta(int idPregunta){
+		this.idPregunta = idPregunta;
+	}
+
+	public int getIdPregunta(){
+		return idPregunta;
+	}
+}
